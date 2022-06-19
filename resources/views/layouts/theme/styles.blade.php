98ffdcd78bb83478facd1d0bfa8419701305149b
Quisbert G <h666heavymetal@gmail.com>
2022-06-19 11:11:16 -0400

    carpetas 2 parte

---
+++ resources/views/layouts/theme/styles.blade.php
@@ -0,0 +1,9 @@
+<!--     Fonts and icons     -->
+<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
+
+<!-- Nucleo Icons -->
+<link href="{{ asset('assets/css/nucleo-icons.css') }}" rel="stylesheet" />
+<link href="{{ asset('assets/css/nucleo-svg.css') }}" rel="stylesheet" />
+<link href="{{ asset('assets/css/nucleo-svg.css') }}" rel="stylesheet" />
+<!-- CSS Files -->
+<link id="pagestyle" href="{{ asset('assets/css/argon-dashboard.css?v=2.0.3') }}" rel="stylesheet" />
